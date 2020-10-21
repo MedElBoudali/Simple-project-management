970e2e236db1d7d60e09adc77dc3433997cbc78e
MedEBoudali <med.sneaky@gmail.com>
2020-10-21 22:22:37 +0000

    add various import syntaxes

--- src/Components/ProjectList.ts
+++ src/Components/ProjectList.ts
@@ -1,13 +1,13 @@
 import { Component } from './BaseComponent.js';
 import { ProjectItem } from '../Components/ProjectItem.js';
-import { autobind } from '../Decorators/Autobind.js';
-import { Project, ProjectStatus } from '../Models/Project.js';
+import { autobind as Autobind } from '../Decorators/Autobind.js';
+import * as Project from '../Models/Project.js';
 import { DragTarget } from '../Models/DragDrop.js';
 import { projectState } from '../State/ProjectState.js';
 
 // Project List Class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
-  assignedProjects: Project[];
+  assignedProjects: Project.Project[];
 
   constructor(private type: 'active' | 'finished') {
     super('project-list', 'app', false, `${type}-projects`);
@@ -16,7 +16,7 @@ export class ProjectList extends Component<HTMLDivElement, HTMLElement> implemen
     this.renderContent();
   }
 
-  @autobind
+  @Autobind
   dragOverHandler(event: DragEvent) {
     if (event.dataTransfer && event.dataTransfer.types[0] == 'text/plain') {
       event.preventDefault();
@@ -24,15 +24,15 @@ export class ProjectList extends Component<HTMLDivElement, HTMLElement> implemen
       listEl.classList.add('droppable');
     }
   }
-  @autobind
+  @Autobind
   dropHandler(event: DragEvent) {
     const prjId = event.dataTransfer!.getData('text/plain');
     projectState.moveProject(
       prjId,
-      this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished
+      this.type === 'active' ? Project.ProjectStatus.Active : Project.ProjectStatus.Finished
     );
   }
-  @autobind
+  @Autobind
   dragLeaveHandler(_: DragEvent) {
     const listEl = this.element.querySelector('ul')! as HTMLUListElement;
     listEl.classList.remove('droppable');
@@ -43,12 +43,12 @@ export class ProjectList extends Component<HTMLDivElement, HTMLElement> implemen
     this.element.addEventListener('drop', this.dropHandler);
     this.element.addEventListener('dragleave', this.dragLeaveHandler);
 
-    projectState.addListeners((projects: Project[]) => {
+    projectState.addListeners((projects: Project.Project[]) => {
       const relevantProjects = projects.filter(project => {
         if (this.type === 'active') {
-          return project.status === ProjectStatus.Active;
+          return project.status === Project.ProjectStatus.Active;
         } else {
-          return project.status === ProjectStatus.Finished;
+          return project.status === Project.ProjectStatus.Finished;
         }
       });
       this.assignedProjects = relevantProjects;
