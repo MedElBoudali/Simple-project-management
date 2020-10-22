05373986f44d6902b11672b3a3680452fc4a5467
MedEBoudali <med.sneaky@gmail.com>
2020-10-22 22:54:53 +0000

    all js files deleted

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+import path from 'path';
+
 module.exports = {
-    
+  entry: './src/app.ts',
+  output: { filename: 'bundle.js', path: path.resolve(__dirname, 'dist') },
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devtool: 'inline-source-map'
 };
