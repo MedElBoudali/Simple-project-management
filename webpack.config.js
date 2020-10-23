14175a1cec7d1f602f23e34faf73c3381a766fd9
MedEBoudali <med.sneaky@gmail.com>
2020-10-23 00:21:35 +0000

    add pro webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
   output: { filename: 'bundle.js', path: path.resolve(__dirname, 'dist') },
   module: {
